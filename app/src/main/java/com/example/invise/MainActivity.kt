68c18fef0ff112c3156125e9982e9f36b6e58bd5
tomikartemik <artemules@mail.ru>
2021-12-21 17:10:02 +0300

    Id

--- app/src/main/java/com/example/invise/MainActivity.kt
+++ app/src/main/java/com/example/invise/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.invise
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.invise.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.*
+
 
 lateinit var binding: ActivityMainBinding
-lateinit var auth: FirebaseAuth
 lateinit var databaseReference: DatabaseReference
 
 class MainActivity : AppCompatActivity() {
@@ -24,19 +21,40 @@ class MainActivity : AppCompatActivity() {
         binding.btn.setOnClickListener {
             val et = binding.editTextName.text.toString()
             if(et == "") {
-                Toast.makeText(this, "Вы не ввели имя", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this , "Вы не ввели имя" , Toast.LENGTH_SHORT).show()
             }
             else{
-                database()
-                startActivity(Intent(this, HomeActivity::class.java))
+                //database(et)
+                getId(et)
+                startActivity(Intent(this , HomeActivity::class.java))
             }
             }
         }
 
     }
-    fun database(){
-        val database = Firebase.database
-        val myRef = database.getReference("message")
-
-        myRef.setValue("Hello, World!")
+    fun database(name : String){
+        val ref = FirebaseDatabase.getInstance().reference.child("Users").child(name).push()
+        ref.child("Id").setValue(0)
+    }
+/////////////Получение Id последнего добавленного пользователя и добавление нового/////////////////////////////////////////////////////////////////////////
+    fun getId(name : String){
+        val databaseReference = FirebaseDatabase.getInstance().reference
+        val lastQuery = databaseReference.child("Users").orderByKey()
+        lastQuery.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot : DataSnapshot) {
+                val message = dataSnapshot.value.toString()
+                val ide = message.indexOf("Id=", 0, false)+3
+                val id = message.indexOf("}", ide, false)
+                val tid = message.substring(ide, id).toInt()
+                Log.d("String", message)
+                Log.d("Ide", ide.toString())
+                Log.d("Id", id.toString())
+                Log.d("Substring", tid.toString())
+                val ref = FirebaseDatabase.getInstance().reference.child("Users").child(name).push()
+                ref.child("Id").setValue(tid+1)
+            }
+            override fun onCancelled(databaseError : DatabaseError) {
+                // Handle possible errors.
+            }
+        })
     }
