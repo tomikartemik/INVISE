46561eb7ead05a55c34986189f126388a7a37866
tomikartemik <artemules@mail.ru>
2021-12-29 01:24:07 +0300

    register

--- app/src/main/java/com/example/invise/MainActivity.kt
+++ app/src/main/java/com/example/invise/MainActivity.kt
@@ -1,60 +1,108 @@
 package com.example.invise
 
 import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
-import android.os.Message
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.invise.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import kotlin.properties.Delegates
 
 
 lateinit var binding: ActivityMainBinding
 lateinit var databaseReference: DatabaseReference
+lateinit var message: String
+var ide by Delegates.notNull<Int>()
+var id by Delegates.notNull<Int>()
+var tid by Delegates.notNull<Int>()
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btn.setOnClickListener {
-            val et = binding.editTextName.text.toString()
-            if(et == "") {
+        val auth = FirebaseAuth.getInstance()
+        val database = FirebaseDatabase.getInstance()
+        val databaseReference = database?.reference!!.child("profile")
+        binding.suBtn.setOnClickListener {
+            val ename = binding.editTextName.text.toString()
+            val epass = binding.editTextPass.toString()
+            if(ename == "") {
                 Toast.makeText(this , "Вы не ввели имя" , Toast.LENGTH_SHORT).show()
             }
+            if(epass == "") {
+                Toast.makeText(this , "Вы не ввели пароль" , Toast.LENGTH_SHORT).show()
+            }
             else{
-                //database(et)
-                getId(et)
-                startActivity(Intent(this , HomeActivity::class.java))
+                //database(ename)
+                //addUser(ename)
+                //startActivity(Intent(this , HomeActivity::class.java))
+                auth.createUserWithEmailAndPassword(ename+"@gmail.com", epass)
+                        .addOnCompleteListener {
+                            if(it.isSuccessful){
+                                val currentUser = auth.currentUser
+                                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this, HomeActivity::class.java))
+                            }else{
+                                Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
             }
             }
+
+        binding.siBtn.setOnClickListener {
+            val ename = binding.editTextName.text.toString()
+            val epass = binding.editTextPass.toString()
+            if(ename == "") {
+                Toast.makeText(this , "Вы не ввели имя" , Toast.LENGTH_SHORT).show()
+            }
+            if(epass == "") {
+                Toast.makeText(this , "Вы не ввели пароль" , Toast.LENGTH_SHORT).show()
+            }
+            else{
+                auth.signInWithEmailAndPassword(ename+"@gmail.com", epass)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful){
+                                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this, HomeActivity::class.java))
+                            }else{
+                                Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+            }
+        }
         }
 
     }
-    fun database(name : String){
-        val ref = FirebaseDatabase.getInstance().reference.child("Users").child(name).push()
-        ref.child("Id").setValue(0)
-    }
+/////////////    fun database(name : String){
+/////////////        val ref = FirebaseDatabase.getInstance().reference.child("Users").child(name).push()
+/////////////        ref.child("Id").setValue(0)
+/////////////    }
+/////////////
 /////////////Получение Id последнего добавленного пользователя и добавление нового/////////////////////////////////////////////////////////////////////////
-    fun getId(name : String){
-        val databaseReference = FirebaseDatabase.getInstance().reference
-        val lastQuery = databaseReference.child("Users").orderByKey()
-        lastQuery.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot : DataSnapshot) {
-                val message = dataSnapshot.value.toString()
-                val ide = message.indexOf("Id=", 0, false)+3
-                val id = message.indexOf("}", ide, false)
-                val tid = message.substring(ide, id).toInt()
-                Log.d("String", message)
-                Log.d("Ide", ide.toString())
-                Log.d("Id", id.toString())
-                Log.d("Substring", tid.toString())
-                val ref = FirebaseDatabase.getInstance().reference.child("Users").child(name).push()
-                ref.child("Id").setValue(tid+1)
-            }
-            override fun onCancelled(databaseError : DatabaseError) {
-                // Handle possible errors.
-            }
-        })
-    }
+/////////////    fun addUser(name : String){
+/////////////        val databaseReference = FirebaseDatabase.getInstance().reference
+/////////////        val lastQuery = databaseReference.child("Users").orderByKey()
+/////////////        lastQuery.addListenerForSingleValueEvent(object : ValueEventListener {
+/////////////            override fun onDataChange(dataSnapshot : DataSnapshot) {
+/////////////                message = dataSnapshot.value.toString()
+/////////////                ide = message.indexOf("Id=" , 0 , false) + 3
+/////////////                id = message.indexOf("}" , ide , false)
+/////////////                tid = message.substring(ide , id).toInt()
+/////////////                Log.d("String" , message)
+/////////////                Log.d("Ide" , ide.toString())
+/////////////                Log.d("Id" , id.toString())
+/////////////                Log.d("Substring" , tid.toString())
+/////////////                val ref = FirebaseDatabase.getInstance().reference.child("Users").child(name).push()
+/////////////                ref.child("Id").setValue(tid + 1)
+/////////////            }
+/////////////
+/////////////            override fun onCancelled(databaseError : DatabaseError) {
+/////////////                // Handle possible errors.
+/////////////            }
+/////////////        })
+/////////////    }
