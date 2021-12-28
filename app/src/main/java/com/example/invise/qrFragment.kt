6f9b312d73e95eacc3eb601d9b6119dfd873250b
Тимур <timiks>
2021-12-29 01:31:34 +0300

    color
    # Conflicts:
    #	app/src/main/java/com/example/invise/MainActivity.kt

--- app/src/main/java/com/example/invise/qrFragment.kt
+++ app/src/main/java/com/example/invise/qrFragment.kt
@@ -1,19 +1,25 @@
 package com.example.invise
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.invise.databinding.FragmentQrBinding
 
 class qrFragment : Fragment() {
+    lateinit var binding: FragmentQrBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr, container, false)
+        binding = FragmentQrBinding.inflate(inflater)
+        binding.but.setOnClickListener{
+            startActivity(Intent(this@qrFragment.context , QrGenerate::class.java))
+        }
+        return binding.root
     }
 
     companion object {
